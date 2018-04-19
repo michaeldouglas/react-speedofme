025cb55d84fd5509c8b0ad271328fa4e95dd4b7e
Michael Douglas Barbosa Araujo <michaeldouglas010790@gmail.com>
2018-04-19 19:20:15 -0300

    Adjust version and keywords

--- index.js
+++ index.js
@@ -2,11 +2,7 @@ import { Component } from 'react';
 import PropTypes from 'prop-types';
 
 export default class SpeedOfMe extends Component {
-    render() {
-        return(
-            <div>
-                Teste
-            </div>
-        );
-      }
+  render () {
+    return <div>Teste</div>;
+  }
 }
