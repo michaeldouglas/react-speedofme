df8fc7ec681bd772c550ed7b517d6f80c9d100b3
Michael Douglas Barbosa Araujo <michaeldouglas010790@gmail.com>
2018-04-19 18:48:56 -0300

    Create structure component

---
+++ index.js
@@ -0,0 +1,12 @@
+import { Component } from 'react';
+import PropTypes from 'prop-types';
+
+export default class SpeedOfMe extends Component {
+    render() {
+        return(
+            <div>
+                Teste
+            </div>
+        );
+      }
+}
---
+++ readme.md
@@ -0,0 +1,2 @@
+# React SpeedOf.Me Component
+
