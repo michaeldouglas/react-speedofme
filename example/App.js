89eb933c8dd0693fcd47c4f54b79528c30c17c82
Michael Douglas Barbosa Araujo <michaeldouglas010790@gmail.com>
2018-04-20 18:36:52 -0300

    Adjust of file ignore. Example Created for component test

---
+++ example/App.js
@@ -0,0 +1,8 @@
+import React, { Component } from 'react';
+import SpeedOfMe from './../dist/index';
+
+export default class App extends Component {
+  render () {
+    return <div>Teste</div>;
+  }
+}
