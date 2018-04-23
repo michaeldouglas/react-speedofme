a45329bfba9e81b2343c1c9cbdd734ae61b2b4d8
Michael Douglas Barbosa Araujo <michaeldouglas010790@gmail.com>
2018-04-23 16:31:16 -0300

    Adjust example call component

--- example/App.js
+++ example/App.js
@@ -1,8 +1,24 @@
 import React, { Component } from 'react';
 import SpeedOfMe from './../dist/index';
 
+import './app.scss';
+
 export default class App extends Component {
   render () {
-    return <div>Teste</div>;
+    return (
+      <div id='app-speedofme'>
+        <SpeedOfMe
+          segments={2}
+          textColor='#FFF'
+          startColor='#F4552C'
+          endColor='#F9F9F9'
+          needleColor='#989898'
+          value={333}
+          needleTransitionDuration={4000}
+          needleTransition='easeElastic'
+          currentValueText='50'
+        />
+      </div>
+    );
   }
 }
---
+++ example/webpack.config.js
@@ -0,0 +1,46 @@
+const path = require('path');
+
+module.exports = {
+  mode: 'development',
+  entry: {
+    index: path.resolve(__dirname, './index.js')
+  },
+  output: {
+    path: __dirname,
+    filename: 'bundle.js'
+  },
+  resolve: {
+    extensions: ['.js', '.jsx', '.ts', '.tsx']
+  },
+  module: {
+    rules: [
+      {
+        test: /\.jsx?$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader'
+        }
+      },
+      {
+        test: /\.tsx?$/,
+        loaders: ['babel-loader', 'ts-loader'],
+        exclude: /node_modules/
+      },
+      {
+        test: /\.scss$/,
+        use: [
+          {
+            loader: 'style-loader'
+          },
+          {
+            loader: 'css-loader'
+          },
+          {
+            loader: 'sass-loader'
+          }
+        ]
+      }
+    ]
+  },
+  devtool: 'inline-source-map'
+};
